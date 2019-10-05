fd38b784ddfe7be400f7751bf91742ba7a9146c9
github username <github email address>
2019-10-05 14:08:46 -0400

    finished js and cleaned up the code

--- day1Drumkit/js/index.js
+++ day1Drumkit/js/index.js
@@ -1,3 +1,26 @@
-window.addEventListener('keydown', function(e) {
-    console.log(e.keyCode)
-})
+
+const keys = document.querySelectorAll('.key');
+
+
+//helper functions
+
+function playSound(e) {
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(!audio) return // stops the function from running
+    audio.currentTime = 0; // rewinds to the start of the sound
+    key.classList.add("playing");
+    audio.play();
+    //console.log(key)
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; // skip it if its not a transform effect
+    this.classList.remove("playing");
+}
+
+//eventListeners
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', playSound)
